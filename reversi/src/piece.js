f97f6b8d95952bfbca21af02443ed42168f8723a
theonewei <wei.n.isaac@gmail.com>
2021-06-09 14:03:40 -0400

    finish make grid

--- reversi/src/piece.js
+++ reversi/src/piece.js
@@ -1,19 +1,25 @@
 /**
  * Initializes the Piece with its color.
  */
-function Piece (color) {
+function Piece(color) {
+  this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+  if (this.color === "white") {
+    return "black";
+  }
+  return "white";
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+  this.color = this.oppColor();
 };
 
 /**
@@ -21,10 +27,14 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+  if (this.color === "white") {
+    return "W";
+  }
+  return "B";
 };
 
 // DON'T TOUCH THIS CODE
-if (typeof window === 'undefined'){
-    module.exports = Piece;
+if (typeof window === "undefined") {
+  module.exports = Piece;
 }
 // DON'T TOUCH THIS CODE
